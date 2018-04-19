41e88bbfc0c5085bbd1ae3b7ebc4572b17decf99
Lukas Buenger <lukasbuenger@gmail.com>
2018-04-19 18:06:14 +0200

    index uses new serializer

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import { Value } from 'slate'
-import { create as createDeserialize } from '../lib/transforms/mdastToSlate'
 import Editor from '../components/Editor'
 
 import initial from './usa'
 
-import {
-  plugins,
-  rules
-} from '../components/Templates/Article'
-
-const deserialize = createDeserialize(
-  rules.map(r => r.fromMdast)
-)
+import { plugins } from '../components/Templates/article'
+import Article from '../lib/rules'
+import { deserialize } from '../lib/serializer'
 
 export default () => (
   <Editor
     plugins={plugins}
     value={Value.fromJSON({
-      document: deserialize(initial)
+      document: deserialize(
+        Article.fromMdast,
+        initial
+      )
     })}
   />
 )
