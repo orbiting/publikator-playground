c7cdf0f136552cf0fe334babef166892a2891c5f
Lukas Buenger <lukasbuenger@gmail.com>
2018-04-20 18:22:33 +0200

    remove unused index reducer

--- components/Editor/reducers/index.js
+++ components/Editor/reducers/index.js
@@ -1,15 +1,7 @@
 import ui from './ui'
 import value from './value'
-import actionTypes from '../constants/actionTypes'
 
 export default {
   ui,
   value
-}
-
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case actionTypes.CHANGE:
-      return ui(value(state, action), action)
-  }
 }
