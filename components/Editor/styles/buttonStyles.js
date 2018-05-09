7b001b3c9624e3f9f8086991fb6ce4c5eae02173
Lukas Buenger <lukasbuenger@gmail.com>
2018-05-09 22:02:27 +0200

    styleguide adaption

--- components/Editor/styles/buttonStyles.js
+++ components/Editor/styles/buttonStyles.js
@@ -2,21 +2,23 @@ import { css } from 'glamor'
 import { colors } from '@project-r/styleguide'
 
 export const iconButton = css({
-  minWidth: '40px',
+  padding: '0 15px',
   minHeight: '40px',
+  color: colors.textColor,
   backgroundColor: '#fff',
   border: 'none',
   display: 'inline-flex',
   alignItems: 'center',
   flexDirection: 'row',
   cursor: 'pointer',
-  transition: 'color .3s',
+  transition: 'color .2s, background-color 0.1s',
   '&[disabled]': {
     cursor: 'default',
     color: colors.disabled
   },
   '&[data-active="true"]': {
-    color: colors.primary
+    backgroundColor: colors.primary,
+    color: '#fff'
   }
 })
 
