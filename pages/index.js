619bd37bda2a9310fee13e73a384adf239490769
Lukas Buenger <lukasbuenger@gmail.com>
2018-08-20 21:38:20 +0200

    dynamic modules

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,40 @@
+import dynamic from 'next/dynamic'
 import React from 'react'
 import { Value } from 'slate'
-import Editor from '@orbiting/publikator-editor'
 import { parse } from '@orbiting/remark-preset'
+import Editor from '@orbiting/publikator-editor'
 import initial from './usa'
 
-import plugins from '@orbiting/publikator-templates/article/plugins'
-import Rule from '@orbiting/publikator-templates/article/rules'
 import { deserialize } from '../lib/serializer'
 
+const Template = dynamic({
+  modules: ({ mdastDocument }) => {
+    switch (mdastDocument.meta.template) {
+      case 'article':
+        return {
+          plugins: import('@orbiting/publikator-templates/article/plugins'),
+          DocumentRule: import('@orbiting/publikator-templates/article/rule'),
+        }
+    }
+  },
+  render: (
+    { mdastDocument },
+    { plugins, DocumentRule }
+  ) => {
+    return (
+      <Editor
+        plugins={plugins}
+        initialValue={Value.fromJSON({
+          document: deserialize(
+            DocumentRule.fromMdast,
+            mdastDocument
+          ),
+        })}
+      />
+    )
+  },
+})
+
 export default () => (
-  <Editor
-    plugins={plugins}
-    initialValue={Value.fromJSON({
-      document: deserialize(
-        Rule.fromMdast,
-        parse(initial)
-      ),
-    })}
-  />
+  <Template mdastDocument={parse(initial)} />
 )
