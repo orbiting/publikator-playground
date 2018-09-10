08d6666260899a6979e7f262f8e9510a92929a58
Lukas Buenger <lukasbuenger@gmail.com>
2018-09-10 18:32:21 +0200

    theme: prevent outline on all buttons

--- packages/editor/apps/theme/buttons.js
+++ packages/editor/apps/theme/buttons.js
@@ -1,46 +1,53 @@
-import { css } from 'glamor'
+import { css, merge } from 'glamor'
 import { colors } from '@project-r/styleguide'
 
-export const iconButton = css({
-  color: colors.textColor,
+export const button = css({
   border: 'none',
-  display: 'inline-flex',
-  alignItems: 'center',
-  flexDirection: 'row',
   cursor: 'pointer',
-  padding: 0,
-  backgroundColor: 'transparent',
-  transition: 'color .2s',
-  '&[disabled]': {
-    cursor: 'default',
-    color: colors.disabled,
-  },
-  '&[data-active="true"]': {
-    color: colors.primary,
-  },
-})
-
-export const labelButton = css({
-  color: colors.textColor,
-  border: 'none',
-  display: 'inline-flex',
   outline: 'none',
-  flexDirection: 'row',
-  cursor: 'pointer',
-  fontSize: '14px',
-  lineHeight: '17px',
-  backgroundColor: 'transparent',
-  transition: 'color .2s, background-color 2s',
-  '&[disabled]': {
-    color: colors.disabled,
-  },
-  '&:hover': {
-    color: colors.primary,
-  },
   '&:active': {
     outline: 'none',
   },
 })
+
+export const iconButton = merge(
+  button,
+  css({
+    color: colors.textColor,
+    display: 'inline-flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 0,
+    backgroundColor: 'transparent',
+    transition: 'color .2s',
+    '&[disabled]': {
+      cursor: 'default',
+      color: colors.disabled,
+    },
+    '&[data-active="true"]': {
+      color: colors.primary,
+    },
+  })
+)
+
+export const labelButton = merge(
+  button,
+  css({
+    color: colors.textColor,
+    display: 'inline-flex',
+    flexDirection: 'row',
+    fontSize: '14px',
+    lineHeight: '17px',
+    backgroundColor: 'transparent',
+    transition: 'color .2s, background-color 2s',
+    '&[disabled]': {
+      color: colors.disabled,
+    },
+    '&:hover': {
+      color: colors.primary,
+    },
+  })
+)
 
 export default () => ({
   iconButton,
