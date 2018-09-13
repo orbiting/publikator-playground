017540640ff453b1950c7aef81528ec0f2e7ccf1
Lukas Buenger <lukasbuenger@gmail.com>
2018-09-13 12:45:31 +0200

    basically working meta

--- components/Plugins/meta/ui.js
+++ components/Plugins/meta/ui.js
@@ -1,74 +1,136 @@
 import { Fragment } from 'react'
 import { compose } from 'ramda'
+import { css } from 'glamor'
 import {
   reduxForm,
   Field as FormField,
 } from 'redux-form'
+import AutosizeInput from 'react-textarea-autosize'
 
 import {
   Field,
   Button,
   Checkbox,
+  Label,
 } from '@project-r/styleguide'
 
 import { withNodeData } from '@orbiting/publikator-editor/apps/nodeData'
 import { withTheme } from '@orbiting/publikator-editor/apps/theme'
 import RepoSearch from '@orbiting/publikator-editor/components/RepoSearch'
+import ImageInput from '@orbiting/publikator-editor/components/ImageInput'
 
-const renderField = ({
-  input,
-  label,
-  type,
-  meta: { touched, error },
-}) => {
-  switch (input.name) {
-    case 'title':
-    case 'slug':
-      return (
-        <Field
-          renderInput={props => (
-            <input
-              autoComplete="off"
-              {...props}
-              {...input}
-            />
-          )}
-          type={type}
-          label={label}
-          {...input}
-          error={touched && error}
-        />
-      )
-    case 'feed':
-    case 'gallery':
-      return (
-        <p>
-          <Checkbox
+const withMetaStyles = withTheme(() => ({
+  autoSize: css({
+    minHeight: 40,
+    paddingTop: '7px !important',
+    paddingBottom: '6px !important',
+  }),
+  imageInputLabel: css({
+    display: 'block',
+    marginBottom: 5,
+  }),
+}))
+
+const renderField = withMetaStyles(
+  ({
+    styles,
+    input,
+    label,
+    type,
+    meta: { touched, error },
+  }) => {
+    switch (input.name) {
+      case 'title':
+      case 'slug':
+        return (
+          <Field
+            renderInput={props => (
+              <input
+                autoComplete="off"
+                {...props}
+              />
+            )}
+            type={type}
+            label={label}
             {...input}
             error={touched && error}
-          >
-            {label}
-          </Checkbox>
-        </p>
-      )
-    case 'format':
-      return (
-        <Fragment>
-          <RepoSearch label="Format" {...input} />
-        </Fragment>
-      )
+          />
+        )
+      case 'feed':
+      case 'gallery':
+        return (
+          <p>
+            <Checkbox
+              {...input}
+              error={touched && error}
+            >
+              {label}
+            </Checkbox>
+          </p>
+        )
+      case 'format':
+      case 'dossier':
+      case 'discussion':
+        return (
+          <Fragment>
+            <RepoSearch
+              label={label}
+              {...input}
+            />
+          </Fragment>
+        )
+      case 'description':
+      case 'facebookDescription':
+      case 'twitterDescription':
+        return (
+          <Field
+            {...input}
+            renderInput={props => (
+              <AutosizeInput
+                {...styles.autoSize}
+                {...props}
+              />
+            )}
+            type={type}
+            label={label}
+            {...input}
+            error={touched && error}
+          />
+        )
+      case 'image':
+      case 'twitterImage':
+      case 'facebookImage':
+        return (
+          <ImageInput {...input}>
+            <Label {...styles.label}>
+              {label}
+            </Label>
+            <img
+              src={
+                input.value ||
+                '/static/placeholder.png'
+              }
+              style={{
+                maxWidth: 200,
+                width: input.value
+                  ? undefined
+                  : '100%',
+              }}
+              alt=""
+            />
+          </ImageInput>
+        )
+    }
   }
-}
+)
 
-export const MetaForm = compose(
+export const MetaUI = compose(
   withNodeData({
     factory: (data, onChange) => ({
       initialValues: data.toJS(),
-      onSubmit: (...args) =>
-        !console.log(args) && onChange(...args),
+      onSubmit: onChange,
     }),
   }),
-  withTheme(),
   reduxForm({
     form: 'meta',
     enableReinitialize: true,
@@ -79,7 +141,6 @@ export const MetaForm = compose(
     pristine,
     reset,
     submitting,
-    styles,
   }) => {
     return (
       <form onSubmit={handleSubmit}>
@@ -87,13 +148,19 @@ export const MetaForm = compose(
           name="title"
           type="text"
           component={renderField}
-          label="Slug"
+          label="Title"
+        />
+        <FormField
+          name="description"
+          type="text"
+          component={renderField}
+          label="Beschreibung"
         />
         <FormField
           name="slug"
           type="text"
           component={renderField}
-          label="Titel"
+          label="Slug"
         />
         <FormField
           name="feed"
@@ -112,16 +179,20 @@ export const MetaForm = compose(
           component={renderField}
           label="Format"
         />
+        <FormField
+          name="discussion"
+          component={renderField}
+          label="Diskussion"
+        />
+
         <div>
           <Button
-            {...styles.buttons.labelButton}
             type="submit"
             disabled={pristine || submitting}
           >
             OK
           </Button>
           <Button
-            {...styles.buttons.labelButton}
             type="button"
             disabled={pristine || submitting}
             onClick={reset}
@@ -133,7 +204,3 @@ export const MetaForm = compose(
     )
   }
 )
-
-export const MetaUI = ({ node, editor }) => {
-  return <MetaForm node={node} editor={editor} />
-}
